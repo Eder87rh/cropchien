3c2c64c7b1bd262b7c709b5377e91a9b0b0e2fa8
Eder <eder.ramirez87@gmail.com>
2019-03-23 12:06:38 -0600

    Creating the generalsubscription firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const axios = require('axios');
+var cors = require('cors') ({origin:true});
+const serverKey = require('./serverKey');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+let firestore = admin.firestore()
+
+exports.GeneralSubscription = functions.https.onRequest((request, response) => {
+    console.log(request.body.token);
+
+    cors(request,response,function(){
+        axios.post(`https://iid.googleapis.com/iid/v1/${request.body.token}/rel/topics/general`,
+        {},
+        {
+            headers: {
+                'Content-Type':'application/json',
+                'Authorization': `key=${serverKey}`
+            },
+        }).then((res) => {
+            console.log(res);
+
+            firestore.collection('tokens').add({
+                token:request.body.token
+            }).then(ref => {
+                console.log('Document added ID: ', ref.id);
+            });
+
+            response.status(200).send(`notifications subscription successful`);
+        }).catch((err) => {
+            console.log(err)
+            console.log(err.response);
+            response.status(500).send({ 
+                message: 'Whops! there was an error',
+                error:err.response
+            });
+        })
+    }); 
+});
